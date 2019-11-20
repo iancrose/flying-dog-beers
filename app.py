f920d1b11ccc3a16f5b2a06f4e21ba938dbd28ed
iancrose <57683003+iancrose@users.noreply.github.com>
2019-11-19 16:06:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,23 @@ app.layout = html.Div([
     # dcc.Link('Go to Source Code', href='{}/code'.format(app_name))
 ], className="container")
 
+@app.callback(
+    dash.dependencies.Output('my-graph', 'figure'),
+    [dash.dependencies.Input('product-selected1', 'value'),
+     dash.dependencies.Input('product-selected2', 'value')])
+def update_graph(selected_product1, selected_product2):
+    dff = df[(df[selected_product1] >= 2) & (df[selected_product2] >= 2)]
+    trace1 = go.Bar(x=dff['state'], y=dff[selected_product1], name=selected_product1.title(), )
+    trace2 = go.Bar(x=dff['state'], y=dff[selected_product2], name=selected_product2.title(), )
+
+    return {
+        'data': [trace1, trace2],
+        'layout': go.Layout(title=f'State vs Export: {selected_product1.title()}, {selected_product2.title()}',
+                            colorway=["#EF963B", "#EF533B"], hovermode="closest",
+                            xaxis={'title': "State", 'titlefont': {'color': 'black', 'size': 14},
+                                   'tickfont': {'size': 9, 'color': 'black'}},
+                            yaxis={'title': "Export price (million USD)", 'titlefont': {'color': 'black', 'size': 14, },
+                                   'tickfont': {'color': 'black'}})}
+
 if __name__ == '__main__':
     app.run_server()
