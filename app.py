5162b5e31ee79cb5031b31fc0f39bc56171d3d25
iancrose <57683003+iancrose@users.noreply.github.com>
2019-11-20 11:58:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import pandas as pd
 tabtitle='Dash Playground'
 
 ########### Load the data
-# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 df = pd.read_csv('https://ianrosewrites.com/1011010/days.csv')
 
 ########### Initiate the app
@@ -25,11 +24,8 @@ app.layout = html.Div([
         figure={
             'data': [
                 go.Scatter(
-                    # x=df[df['continent'] == i]['gdp per capita'],
                     x=df[df['daywk'] == i]['daynum'],
-                    # y=df[df['continent'] == i]['life expectancy'],
                     y=df[df['daywk'] == i]['yes'],
-                    # text=df[df['continent'] == i]['country'],
                     mode='markers',
                     opacity=0.7,
                     marker={
