771b44f56dc2b426150b3b3e6afdecb0d2e87cf4
iancrose <57683003+iancrose@users.noreply.github.com>
2019-11-20 09:40:16 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,10 @@ import plotly.graph_objs as go
 import pandas as pd
 
 ########### Define your variables
-tabtitle='Food Exports by State'
+tabtitle='Dash Playground'
 
 ########### Load the data
-'''
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'c78bf172206ce24f77d6363a2d754b59/raw/'
-    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
-    'usa-agricultural-exports-2011.csv')
-'''
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -24,9 +18,6 @@ server = app.server
 app.title=tabtitle
 
 ########### Set up the layout
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
 app.layout = html.Div([
     dcc.Graph(
         id='life-exp-vs-gdp',
@@ -55,41 +46,6 @@ app.layout = html.Div([
         }
     )
 ])
-
-'''
-app.layout = html.Div([
-    html.H1("Food Product Exports in the United States", style={"textAlign": "center"}),
-    html.Div([html.Div([dcc.Dropdown(id='product-selected1',
-                                     options=[{'label': i.title(), 'value': i} for i in df.columns.values[2:]],
-                                     value="poultry")], className="six columns",
-                       style={"width": "40%", "float": "right"}),
-              html.Div([dcc.Dropdown(id='product-selected2',
-                                     options=[{'label': i.title(), 'value': i} for i in df.columns.values[2:]],
-                                     value='beef')], className="six columns", style={"width": "40%", "float": "left"}),
-              ], className="row", style={"padding": 50, "width": "60%", "margin-left": "auto", "margin-right": "auto"}),
-    dcc.Graph(id='my-graph'),
-
-    # dcc.Link('Go to Source Code', href='{}/code'.format(app_name))
-], className="container")
-
-@app.callback(
-    dash.dependencies.Output('my-graph', 'figure'),
-    [dash.dependencies.Input('product-selected1', 'value'),
-     dash.dependencies.Input('product-selected2', 'value')])
-def update_graph(selected_product1, selected_product2):
-    dff = df[(df[selected_product1] >= 2) & (df[selected_product2] >= 2)]
-    trace1 = go.Bar(x=dff['state'], y=dff[selected_product1], name=selected_product1.title(), )
-    trace2 = go.Bar(x=dff['state'], y=dff[selected_product2], name=selected_product2.title(), )
-
-    return {
-        'data': [trace1, trace2],
-        'layout': go.Layout(title=f'State vs Export: {selected_product1.title()}, {selected_product2.title()}',
-                            colorway=["#EF963B", "#EF533B"], hovermode="closest",
-                            xaxis={'title': "State", 'titlefont': {'color': 'black', 'size': 14},
-                                   'tickfont': {'size': 9, 'color': 'black'}},
-                            yaxis={'title': "Export price (million USD)", 'titlefont': {'color': 'black', 'size': 14, },
-                                   'tickfont': {'color': 'black'}})}
-'''
 
 if __name__ == '__main__':
     app.run_server()
