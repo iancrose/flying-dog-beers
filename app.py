452c2756ff63ac462f6da5a6da15a03968ce27eb
iancrose <57683003+iancrose@users.noreply.github.com>
2019-11-20 09:55:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import pandas as pd
 tabtitle='Dash Playground'
 
 ########### Load the data
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+# df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df = pd.read_csv('https://ianrosewrites.com/1011010/days.csv')
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -24,9 +25,11 @@ app.layout = html.Div([
         figure={
             'data': [
                 go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
+                    # x=df[df['continent'] == i]['gdp per capita'],
+                    x=df[df['daywk'] == i]['daynum'],
+                    # y=df[df['continent'] == i]['life expectancy'],
+                    y=df[df['daywk'] == i]['yes'],
+                    # text=df[df['continent'] == i]['country'],
                     mode='markers',
                     opacity=0.7,
                     marker={
@@ -34,12 +37,12 @@ app.layout = html.Div([
                         'line': {'width': 0.5, 'color': 'white'}
                     },
                     name=i
-                ) # for i in df.continent.unique()
+                ) for i in df.daywk.unique()
             ],
             'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                xaxis={'title': 'Day #'},
+                yaxis={'title': 'Hours Logged'},
+                margin={'l': 0, 'b': 0, 't': 0, 'r': 0},
                 legend={'x': 0, 'y': 1},
                 hovermode='closest'
             )
